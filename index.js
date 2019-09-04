4a46b77c0ac4d08b1d002cfc0a7ab379208e3345
Steven Travers <steven.travers20@gmail.com>
2019-09-04 17:14:21 +0200

    basic

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const App = () => {
+    return(
+        <div>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+ReactDom.render(
+    <App/>,
+    document.getElementById('app')
+)
